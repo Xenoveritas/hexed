38e29e5be236c589a3e71ef1e2165ba7a3c5705a
Xenoveritas <dmpotter44@gmail.com>
2015-05-02 13:50:34 -0400

    Add decoded column

--- html/htmlpane.js
+++ html/htmlpane.js
@@ -1,5 +1,8 @@
 var hexfile = require('./hex-file');
 
+// Limit the maximum size file we'll load to 4MB.
+var MAX_SAFE_INDEX = 1024;
+
 function FilePane(id, filename) {
   var container = document.createElement('div');
   this._container = container;
@@ -32,31 +35,72 @@ FilePane.prototype = {
     var s = byte.toString(16).toUpperCase();
     return s.length == 1 ? "0" + s : s;
   },
+  /**
+   * Translates a byte into a "decoded" form. At some point this is probably
+   * going to be updated to do UTF-8 decoding (because that makes more sense
+   * these days) but for now it only does ASCII values. Any character that's
+   * out of range is returned as '.'. (This may be changed to be null in the
+   * future so it can be rendered differently in the output.)
+   */
+  _decodeByte: function(byte) {
+    if (byte < 32) {
+      return '.';
+    } else if (byte < 128) {
+      return String.fromCharCode(byte);
+    } else {
+      return '.';
+    }
+  },
   _appendChunk: function(index, chunk) {
     if (this._loadingIndicator) {
       // Remove the loading indicator
-      this._loadingIndicator.parentNode.removeChild(this._loadingIndicator);
+      this._loadingIndicator.remove();
       this._loadingIndicator = null;
     }
-    var div = document.createElement('div');
-    div.className = 'chunk';
-    this._container.appendChild(div);
-    var offset = index * hexfile.CHUNK_SIZE, line, html, linenum;
+    if (!this._gutterDiv) {
+      // Create the various components of the view that haven't been generated
+      // yet.
+      this._gutterDiv = document.createElement('div');
+      this._gutterDiv.className = 'gutter';
+      this._hexDiv = document.createElement('div');
+      this._hexDiv.className = 'hex';
+      this._decodeDiv = document.createElement('div');
+      this._decodeDiv.className = 'decode';
+      this._container.appendChild(this._gutterDiv);
+      this._container.appendChild(this._hexDiv);
+      this._container.appendChild(this._decodeDiv);
+    }
+    var offset = index * hexfile.CHUNK_SIZE, line, hex, decoded, linenum;
     for (var i = 0; i < chunk.length;) {
       line = document.createElement('div');
-      line.className = 'bytes';
-      html = '<span class="linenum">' + (offset + i).toString(16).toUpperCase() + '</span>';
+      line.className = 'offset-number';
+      line.innerText = (offset + i).toString(16).toUpperCase();
+      this._gutterDiv.appendChild(line);
+      hex = '';
+      decoded = '';
       for (var j = 0; i < chunk.length && j < 16; i++, j++) {
-        html += this._formatByte(chunk[i]) + " ";
+        hex += this._formatByte(chunk[i]) + " ";
+        decoded += this._decodeByte(chunk[i]);
       }
-      line.innerHTML = html;
-      div.appendChild(line);
+      line = document.createElement('div');
+      line.className = 'line';
+      line.innerText = hex;
+      this._hexDiv.appendChild(line);
+      line = document.createElement('div');
+      line.className = 'line';
+      line.innerText = decoded;
+      this._decodeDiv.appendChild(line);
     }
   },
   /**
-   * Load the given chunk. Files are assumed to be in chunks.
+   * Load the given chunk. Files are organized into chunks of CHUNK_SIZE as far
+   * as we're concerned.
    */
   loadChunk: function(index) {
+    if (index > MAX_SAFE_INDEX) {
+      alert("File size is too large, truncating.");
+      return;
+    }
     var me = this;
     this.file.readChunk(index, function(err, buffer) {
       if (buffer != null) {
