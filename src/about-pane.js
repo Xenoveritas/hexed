ed366279dc9a724bbc6cab84f2cce099fa6a99b3
Xenoveritas <dmpotter44@gmail.com>
2017-12-17 01:46:36 -0500

    Update about pane

--- src/about-pane.js
+++ src/about-pane.js
@@ -1,9 +1,22 @@
+let version = null;
+
+function getVersion() {
+  if (version != null) {
+    return version;
+  } else {
+    let packageJson = require('../package.json');
+    version = packageJson['version'];
+    return version;
+  }
+}
+
 /**
  * Simple module for displaying about information.
  */
 function AboutPane(pane) {
   pane.title = 'About';
-  pane.contents.innerHTML = '<h1>Hexed</h1><p>Hexed hexidecimal file viewer.</p><p>Running on io.js ' + process.version + ' and Electron ' + process.versions['electron'] + '.</p>';
+  pane.contents.className += ' about';
+  pane.contents.innerHTML = '<h1>Hexed ' + getVersion() + '</h1><p>Hexed file viewer.</p><p>Built using Electron ' + process.versions['electron'] + '.</p>';
 }
 
 module.exports = AboutPane;
